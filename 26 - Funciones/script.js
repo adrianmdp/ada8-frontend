cfb3920cdb036806d4d0dd413f410815f39bc7a1
Adrián <adrian@manjaro.fibertel.com.ar>
2021-04-10 09:50:33 -0300

    Actualización de clases

---
+++ 26 - Funciones/script.js
@@ -0,0 +1,94 @@
+// Estructura de una función regular
+
+// function nombre(params1, param2, paramN) {
+//     Implementación
+// }
+
+/**
+ * Funcion regular
+ */
+function mostrarFecha() {
+  console.log(new Date());
+}
+
+mostrarFecha();
+
+/**
+ * Funcion regular con parámetros.
+ */
+function mostrarEnConsola(param1, param2, param3) {
+  console.log(`Mi nombre es ${param1}, soy ${param2}, vivo en ${param3}`);
+}
+
+let nombre = prompt("Ingrese su nombre");
+
+mostrarEnConsola(nombre, "programador", "Argentina");
+
+let valor = 123;
+
+console.log(valor * 3);
+
+// Acá continué con mi código hasta que la volví a necesitar
+
+mostrarEnConsola("Doctor", "Uruguay");
+
+console.log("-----------------------------------------------------------");
+
+function suma(valor1, valor2) {
+  console.log(valor1 + valor2);
+}
+
+suma(3, 9);
+
+console.log("-----------------------------------------------------------");
+
+/**
+ * Funcion anónica autoinvocada
+ */
+(function () {
+  console.log("Esta es una función anónima");
+})();
+
+/**
+ * Asignación de unciones a variables
+ */
+
+// Tenemos una funcion anónima que se esta asignando a una variable.
+// Para invocar a esta funcion, llamo a la variable con parenstesis.
+let productoVar = function (valor1, valor2, valor3) {
+  console.log(valor1 * valor3 * valor2);
+};
+
+productoVar(3, 6, 5);
+
+/**
+ * Funciones de flecha (arrow functions)
+ */
+const myArrowFunction = (valor3, valor1, valor2) => {
+  console.log(valor1 * valor2 * valor3);
+};
+
+myArrowFunction(2, 3, 4);
+
+/**
+ * Retorno de valores
+ */
+
+console.log(
+  "---------------------Retorno de valores--------------------------------------"
+);
+
+const saludar = (nombre) => {
+  console.log(`Hola ${nombre}!!!`);
+};
+
+saludar("Adrián");
+
+const sumar = (valor1, valor2) => {
+  const rta = valor1 + valor2;
+  return rta;
+};
+
+const resultado = sumar(4.4, 6.1);
+
+console.log(resultado);
---
+++ 27 - Nativas/index.js
@@ -0,0 +1,42 @@
+// length
+// La propiedad length de un objeto String representa la longitud de una cadena, en unidades de código.
+
+// toUpperCase
+// El toUpperCase() método devuelve el valor convertido en mayúsculas de la cadena que realiza la llamada.
+
+// toLowerCase
+// El método toLowerCase() devuelve el valor en minúsculas de la cadena que realiza la llamada.
+
+// slice
+// El método slice() extrae una sección de una cadena y devuelve una cadena nueva.
+
+// includes
+// El método includes() determina si una cadena de texto puede ser encontrada dentro de otra cadena de texto, devolviendo true o false según corresponda.
+
+// indexOf
+// El método indexOf()devuelve el índice, dentro del objeto String que realiza la llamada, de la primera ocurrencia del valor especificado, comenzando la búsqueda desde indiceDesde; o -1 si no se encuentra dicho valor.
+
+// replace
+// El método replace() devuelve una nueva cadena con algunas o todas las coincidencias de un patrón, siendo cada una de estas coincidencias reemplazadas por remplazo. El patrón puede ser una cadena o una RegExp, y el reemplazo puede ser una cadena o una función que será llamada para cada coincidencia. Si el patrón es una cadena, sólo la primera coincidencia será reemplazada.
+// La cadena original permanecerá inalterada.
+
+// toString
+// Devuelve una cadena que representa al objeto Number especificado
+
+// min
+// Obtiene el minimo de una serie de números
+
+// max
+// Obtiene el maximo de una serie de números
+
+// round
+// Redondea hacia arriba si el decimal esta por encima de 49 y hacia abajo por debajo de 50.
+
+// ceil
+// Redondea hacia arriba sin importar el valor del decimal.
+
+// floor
+// Redondea hacia abajo sin importar el valor del decimal.
+
+// random
+// devuelve un valor aleatorio
