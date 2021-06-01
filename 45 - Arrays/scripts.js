c2ae3a3abd67068ce4d933c7772c77c58d60503f
Adrián <jasmdp@hotmail.com>
2021-06-01 18:25:34 -0300

    Actualización de contenidos

---
+++ 45 - Arrays/scripts.js
@@ -0,0 +1,39 @@
+/**
+ * pop
+ * El método pop() elimina el último elemento de un array y lo devuelve. Este método cambia la longitud del array.
+ * */
+
+/**
+ * shift
+ * El método shift() elimina el primer elemento del array y lo retorna. Este método modifica la longitud
+ */
+
+/**
+ * push
+ * El método push() añade uno o más elementos al final de un array y devuelve la nueva longitud del array.
+ */
+
+/**
+ * unshift
+ * El método unshift() agrega uno o más elementos al inicio del array, y devuelve la nueva longitud del array.
+ */
+
+/**
+ * slice
+ * El método slice() devuelve una copia de una parte del array dentro de un nuevo array empezando por inicio hasta fin (fin no incluido). El array original no se modificará.
+ */
+
+/**
+ * splice
+ * El método splice() cambia el contenido de un array eliminando elementos existentes y/o agregando nuevos elementos
+ */
+
+/**
+ * concat
+ * El método concat() se usa para unir dos o más arrays. Este método no cambia los arrays existentes, sino que devuelve un nuevo array.
+ */
+
+/**
+ * join
+ * El método join() une todos los elementos de una matriz (o un objeto similar a una matriz) en una cadena y devuelve esta cadena.
+ */
